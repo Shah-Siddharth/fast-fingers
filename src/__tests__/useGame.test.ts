a4579a08870b8baefe682cd476241d4bf63245f5
Siddharth Shah <siddharth.shah@mail.com>
2024-01-24 16:43:41 +0530

    format test files

--- src/__tests__/useGame.test.ts
+++ src/__tests__/useGame.test.ts
@@ -2,16 +2,18 @@ import { renderHook, waitFor } from "@testing-library/react";
 import useGame from "../hooks/useGame";
 import { difficultyType, gameStateType } from "../types";
 
-const setDifficultyMock = jest.fn((_difficulty) => { });
-const onGameOverMock = jest.fn(() => { });
+const setDifficultyMock = jest.fn((_difficulty) => {});
+const onGameOverMock = jest.fn(() => {});
 
 function setup(difficulty: difficultyType, gameState: gameStateType) {
-  const { result } = renderHook(() => useGame({
-    difficulty,
-    gameState,
-    setDifficulty: setDifficultyMock,
-    onGameOver: onGameOverMock
-  }));
+  const { result } = renderHook(() =>
+    useGame({
+      difficulty,
+      gameState,
+      setDifficulty: setDifficultyMock,
+      onGameOver: onGameOverMock,
+    })
+  );
 
   return { ...result.current };
 }
@@ -24,8 +26,8 @@ describe("useGame hook", () => {
   });
 
   test("hook should call onGameOver() when time runs out", () => {
-    const { remainingTime } = setup('Easy', 'PLAYING');
+    const { remainingTime } = setup("Easy", "PLAYING");
     waitFor(() => expect(remainingTime).toBe(0))
       .then(() => expect(onGameOverMock.mock.calls.length).toBe(1));
-  })
-})
+  });
+});
--- src/__tests__/useTimer.test.ts
+++ src/__tests__/useTimer.test.ts
@@ -2,8 +2,14 @@ import { renderHook, waitFor } from "@testing-library/react";
 import useTimer from "../hooks/useTimer";
 import { gameStateType } from "../types";
 
-function setup(currentWord: string, difficultyFactor: number, gameState: gameStateType) {
-  const { result } = renderHook(() => useTimer(currentWord, difficultyFactor, gameState));
+function setup(
+  currentWord: string,
+  difficultyFactor: number,
+  gameState: gameStateType,
+) {
+  const { result } = renderHook(() =>
+    useTimer(currentWord, difficultyFactor, gameState)
+  );
   const [maxTimeForWord, remainingTime, gameOver] = result.current;
 
   return { maxTimeForWord, remainingTime, gameOver };
@@ -13,14 +19,18 @@ describe("useTimer hook", () => {
   test("hook should contain correct initial values", () => {
     const currentWord: string = "word";
     const difficultyFactor: number = 1;
-    const { maxTimeForWord, gameOver } = setup(currentWord, difficultyFactor, 'PLAYING');
+    const { maxTimeForWord, gameOver } = setup(
+      currentWord,
+      difficultyFactor,
+      "PLAYING",
+    );
     expect(maxTimeForWord).toEqual(currentWord.length / difficultyFactor);
     expect(gameOver).toEqual(false);
   });
 
   test("gameover should be set to true once timer runs out", () => {
-    const { remainingTime, gameOver } = setup("abcd", 1, 'PLAYING');
+    const { remainingTime, gameOver } = setup("abcd", 1, "PLAYING");
     waitFor(() => expect(remainingTime).toEqual(0))
       .then(() => expect(gameOver).toBe(true));
   });
-})
+});
